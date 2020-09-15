8eec4b4fc0e9a0dec7a04733e73046d0d421d9fd
Artur <test@test.com>
2020-09-16 01:15:37 +0300

    branch fix

--- src/test/java/tests/TestBaseSetup.java
+++ src/test/java/tests/TestBaseSetup.java
@@ -6,18 +6,12 @@ import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.firefox.FirefoxOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.ITestContext;
-import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
-import test.java.pages.ContactPage;
-import test.java.pages.HomePage;
-import test.java.pages.QaPage;
-import test.java.utils.Screenshot;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -29,18 +23,18 @@ public class TestBaseSetup {
     @Parameters({ "browser" })
     @BeforeMethod
     public void setUp(@Optional("chrome") String browser) {
-        System.setProperty("webdriver.chrome.driver", "chromedriver.ex
+        System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
         //FirefoxOptions ffOptions = new FirefoxOptions();
         driver = new ChromeDriver(options);
-        /*try {
+        try {
             driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options);
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
-        }*/
+        }
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(1, TimeUnit.MILLISECONDS);
     }
